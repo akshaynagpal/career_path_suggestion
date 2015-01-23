10b8c6f87499b6e2d44073fb17547c15dd7c007e
Akshay Nagpal <akshay2626@gmail.com>
2015-01-23 18:22:21 +0530

    updating

--- fuzzy_quzzy_snippets.py
+++ fuzzy_quzzy_snippets.py
@@ -88,3 +88,45 @@ Software Engineer->88
 0.9230769230769231
 >>> fuzz.ratio("Data Scientist","Data Analyst")
 62
+
+
+
+--------------------------------------------------------------
+Python 2.7 (r27:82525, Jul  4 2010, 07:43:08) [MSC v.1500 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import csv
+>>> with open("C:\Users\advance\Documents\GitHub\career_path_prediction\Career_data.csv",'rb') as csvfile:
+	filereader = csv.reader(csvfile,delimiter=',')
+
+	
+
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    with open("C:\Users\advance\Documents\GitHub\career_path_prediction\Career_data.csv",'rb') as csvfile:
+IOError: [Errno 22] invalid mode ('rb') or filename: 'C:\\Users\x07dvance\\Documents\\GitHub\\career_path_prediction\\Career_data.csv'
+>>> with open("C:\/Users\/advance\/Documents\/GitHub\/career_path_prediction\/Career_data.csv",'rb') as csvfile:
+	filereader = csv.reader(csvfile,delimiter=',')
+
+	
+>>> cd_list = []
+>>> cd_list
+[]
+>>> for row in filereader:
+	cd_list.append([row])
+
+	
+
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    for row in filereader:
+ValueError: I/O operation on closed file
+>>> with open("C:\/Users\/advance\/Documents\/GitHub\/career_path_prediction\/Career_data.csv",'rb') as csvfile:
+	filereader = csv.reader(csvfile,delimiter=',')
+	for row in filereader:
+		cd_list.append([row])
+
+		
+>>> cd_list
+[[['Bachelors Stream', 'Bachelors Univ', 'Bachelors Duration', 'Masters Stream', 'Masters Univ', 'Masters Duration', 'Doctoral Stream', 'Doctoral Univ', 'Doctoral Duration', 'Work Position', 'Work Organisation']], [['CS', 'Stanford University', '2000-2004', 'CS', 'Princeton University', '2004-2006', 'Data Science', 'MIT', '2006-2009', 'Data Scientist', 'Google']], [['EE', 'Manipal University', '1984-1988', 'CS', 'University of Wisconsin', '1988-1990', '', '', '', 'Software Engineer', 'Microsoft']], [['ME', 'Delhi College of Engineering', '2004-2008', '', '', '', '', '', '', 'Mechanical Design Engineer', 'Ford']], [['IT', 'IIT Delhi', '2001-2005', 'Networking and System Engineering', 'University of California', '2005-2007', '', '', '', 'System Administrator', 'Google']], [['CS', 'IIIT Hyderabad', '1999-2003', 'Software Engineering', 'New York University', '2003-2005', '', '', '', 'Software Developer', 'Yahoo']], [['Mathematics and Computing', 'IIT Delhi', '2001-2005', 'Data Mining', 'NTU Singapore', '2005-2007', 'Statistics', 'Harvard', '2005-2008', 'Data Analyst', 'Quora']], [['CS', 'IIT Delhi', '2001-2005', 'Software Engineering', 'University of Wisconsin', '2005-2007', 'Distributed Systems', 'MIT', '2007-2010', 'Research and Development', 'Microsoft']], [['Mechanical Engineering', 'IIT Kharagpur', '2000-2004', 'Automobile Engineering', 'University of France ', '2004-2006', 'MBA Product Development', 'Harvard', '2009-2011', 'Product Development', 'Mercedes']], [['IT', 'ITM University', '2001-2005', '', 'IIM Ahemdabad', '2005-2007', '', '', '', 'Marketing Manager', 'Airtel']], [['CS', 'Stanford University', '2000-2004', '', '', '', '', '', '', 'CEO', 'Google']], [['Civil Engineering', 'NIT Kurukshetra', '1998-2002', 'Structure Engineering', 'IIT Delhi', '2002-2004', '', '', '', 'Chief Architect', 'LNT']]]
+>>> import fuzzywuzzy
+>>> 
