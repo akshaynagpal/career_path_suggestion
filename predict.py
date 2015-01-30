f5de652a1be468b99580ca0a9a6a475b96d724a4
Akshay Nagpal <akshay2626@gmail.com>
2015-01-30 22:45:23 +0530

    added partial_ratio matching

--- predict.py
+++ predict.py
@@ -14,9 +14,12 @@ with open("C:\/Users\/advance\/Documents\/GitHub\/career_path_prediction\/Career
 data_array = np.array(cd_list)
 
 def predictor():
-        user_future = raw_input("Enter your future profession : ")
+        flag = 0
+        user_input = raw_input("Enter your future profession : ")
+        user_future = user_input.title()
         for i in range(1,data_array.shape[0]):
                 if fuzz.ratio(user_future,data_array[i][9])>60:
+                        flag = 1
                         print "(Relevance = "+ str(fuzz.ratio(user_future,data_array[i][9])) + ") ",
                         if data_array[i][0] != '':
                                 print "Bachelors : "+ data_array[i][0],
@@ -26,6 +29,20 @@ def predictor():
                                 print "-> Doctoral : "+ data_array[i][6],
                         print "-> " + data_array[i][9],
                         print "\n"
+        if flag == 0:
+                print "no simple ratio match...trying for partial match..."
+                for i in range(1,data_array.shape[0]):
+                        if fuzz.partial_ratio(user_future,data_array[i][9])>80:
+                                flag = 1
+                                print "(Relevance = "+ str(fuzz.ratio(user_future,data_array[i][9])) + ") ",
+                                if data_array[i][0] != '':
+                                        print "Bachelors : "+ data_array[i][0],
+                                if data_array[i][3] != '':
+                                        print "-> Masters : "+ data_array[i][3],
+                                if data_array[i][6] != '':
+                                        print "-> Doctoral : "+ data_array[i][6],
+                                print "-> " + data_array[i][9],
+                                print "\n"
 
 choice = 'Y'                     
 while choice == 'Y' or choice == 'y':
